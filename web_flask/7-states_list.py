c12692db85d9abf3e4373e94c16f9c9b0eee972b
nzejoe <jonathannzete@gmail.com>
2024-03-23 08:26:27 -0700

    8. List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,32 +1,43 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
 
-The application listens on 0.0.0.0, port 5000.
+""" Module that includes script that starts a Flask web application.
+Web application must be listening on 0.0.0.0, port 5000
+- must use storage for fetching data from the storage engine:
+  (FileStorage or DBStorage) => from models import storage and storage.all(...)
+After each request you must remove the current SQLAlchemy Session:
+  - Declare a method to handle @app.teardown_appcontext
+  - Call in this method storage.close()
 Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
+    - /states_list: display a HTML page: (inside the tag BODY)
+      - H1 tag: “States”
+      - UL tag: with the list of all State objects present in DBStorage
+        sorted by name (A->Z)
+        - LI tag: description of one State: <state.id>: <B><state.name></B>
+must use the option strict_slashes=False in your route definition
 """
+
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
+from models import *
+from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
+    """Displays a HTML page with a list of states."""
+    sorted_states = sorted(list(storage.all("State").
+                                values()), key=lambda x: x.name)
 
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+    return render_template('7-states_list.html', states=sorted_states)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown(exception):
+    """Removes the current SQLAlchemy Session."""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
