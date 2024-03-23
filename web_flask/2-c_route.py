9f95e27fb890c6c17a36a6aa6520e45ce822717c
nzejoe <jonathannzete@gmail.com>
2024-03-23 07:06:29 -0700

    2. C is fun

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,35 +1,37 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
 
-The application listens on 0.0.0.0, port 5000.
+""" Module that includes script that starts a Flask web application.
+Web application must be listening on 0.0.0.0, port 5000
 Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
-    /c/<text>: Displays 'C' followed by the value of <text>.
+  - /: display “Hello HBNB!”
+  - /hbnb: display “HBNB”
+  - /c/<text>: display “C ” followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+must use the option strict_slashes=False in your route definition
 """
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'."""
-    return "Hello HBNB!"
+    """Function called through the / route."""
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'."""
-    return "HBNB"
+    """Function called through the /hbnb route."""
+    return 'HBNB'
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """Displays 'C' followed by the value of <text>."""
-    text = text.replace("_", " ")
-    return "C {}".format(text)
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """Function called through the /c/<text> route."""
+    return 'C %s' % text.replace('_', ' ')
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
