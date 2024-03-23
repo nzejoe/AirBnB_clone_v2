575abc06e35c1d0d13bb0f08e11acc4ab66ece09
nzejoe <jonathannzete@gmail.com>
2024-03-23 10:39:13 -0700

    9. Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,32 +1,42 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
 
-The application listens on 0.0.0.0, port 5000.
+""" Module that includes script that starts a Flask web application.
+Web application must be listening on 0.0.0.0, port 5000
+- must use storage for fetching data from the storage engine:
+  (FileStorage or DBStorage) => from models import storage and storage.all(...)
+After each request you must remove the current SQLAlchemy Session:
+  - Declare a method to handle @app.teardown_appcontext
+  - Call in this method storage.close()
 Routes:
-    /cities_by_states: HTML page with a list of all states and related cities.
+    - /states_list: display a HTML page: (inside the tag BODY)
+      - H1 tag: “States”
+      - UL tag: with the list of all State objects present in DBStorage
+        sorted by name (A->Z)
+        - LI tag: description of one State: <state.id>: <B><state.name></B>
+must use the option strict_slashes=False in your route definition
 """
+
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
+from models.state import State
+from models.city import City
 
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    """Displays an HTML page with a list of all states and related cities.
-
-    States/cities are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("8-cities_by_states.html", states=states)
-
-
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown_appcontext(exception):
+    """Remove the current SQLAlchemy Session"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Displays a HTML page with a list of States and Cities"""
+    states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
+
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
